a23b00b1d85f62006949a8d2b757122aef2394d4
cofob <cofob@riseup.net>
2022-07-28 02:58:21 -0700

    Rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,9 +2,9 @@ let
   cofob = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETMEzhigdZelWae3V4tQ7/LXsub39SRG2X+jPMeoHMx";
   users = [ cofob ];
 
-  bear = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPBVz/lhiMam5Pz39QtS4Gm13gIVqdrtnlVSeuS/+7Wg";
+  bear = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFQKu0ZgZc02N+HguIs4Ur50p6A4l9SxpRWGxsQGcViN";
   systems = [ bear ];
 in
 {
-  "secrets/passwords/cofob.age".publicKeys = users ++ systems;
+  "passwords/cofob.age".publicKeys = users ++ systems;
 }
