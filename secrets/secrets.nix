284c1b537993fed106606a13aac7f2623141a1a8
cofob <cofob@riseup.net>
2022-07-28 12:41:16 +0200

    Change bear host key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   cofob = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETMEzhigdZelWae3V4tQ7/LXsub39SRG2X+jPMeoHMx";
   users = [ cofob ];
 
-  bear = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFQKu0ZgZc02N+HguIs4Ur50p6A4l9SxpRWGxsQGcViN";
+  bear = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJxE1dSCkVnBcjNM1dvEEEEvxZ4F+FkOr6fVh+V9wotS";
   systems = [ bear ];
 in
 {
