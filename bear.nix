195913cd49899d140bfd56caddd39c2959efe6ba
cofob <cofob@riseup.net>
2022-08-01 01:02:49 +0700

    Enable IPv6 for yggdrasil in i2pd

--- bear.nix
+++ bear.nix
@@ -34,6 +34,7 @@
 
     i2pd = {
       enable = true;
+      enableIPv6 = true;
       proto.http.enable = true;
       proto.httpProxy.enable = true;
       proto.httpProxy.outproxy = "http://acetone.i2p:8888";
