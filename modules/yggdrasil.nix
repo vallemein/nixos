5877f0bb6016b05fdb24c28292bdc11126653174
cofob <cofob@riseup.net>
2022-08-02 19:24:29 +0700

    Add more yggdrasil peers

--- modules/yggdrasil.nix
+++ modules/yggdrasil.nix
@@ -10,6 +10,15 @@
         Peers = [
           "tcp://kazi.peer.cofob.ru:18000"
           "tls://ygg.averyan.ru:8362"
+          "tcp://94.130.203.208:5999"
+          "tcp://ygg.loskiq.dev:17313"
+          "tcp://itcom.multed.com:7991"
+          "tls://ygg-ny-us.incognet.io:8884"
+          "tcp://tasty.chowder.land:9002"
+          "tcp://ygg-nv-us.incognet.io:8883"
+          "tls://54.37.137.221:11129"
+          "tcp://ygg.mkg20001.io:80"
+          "tls://ygg.mkg20001.io:443"
         ];
 
         IfName = "ygg0";
