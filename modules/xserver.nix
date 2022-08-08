8e9e2e3b26cd28cbb0e4b236292bd313e0f30b8f
cofob <cofob@riseup.net>
2022-08-08 22:02:18 +0700

    Disable default packages in gnome

--- modules/xserver.nix
+++ modules/xserver.nix
@@ -2,18 +2,35 @@
 
 {
   config = {
+    environment.systemPackages = with pkgs.unstable; [
+      gnome-console
+      gnomeExtensions.gsconnect
+      gnomeExtensions.yakuake
+    ];
+
     nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
       "nvidia-x11"
       "nvidia-settings"
     ];
 
-    services.xserver = {
-      desktopManager.gnome.enable = true;
-      displayManager.gdm.enable = true;
-      xkbOptions = "grp:caps_toggle";
-      videoDrivers = [ "nvidia" ];
-      layout = "us,ru";
-      enable = true;
+    services = {
+      xserver = {
+        desktopManager.gnome.enable = true;
+        displayManager.gdm.enable = true;
+        xkbOptions = "grp:caps_toggle";
+        videoDrivers = [ "nvidia" ];
+        layout = "us,ru";
+        enable = true;
+      };
+
+      gnome = {
+        core-utilities.enable = false;
+        tracker-miners.enable = false;
+        tracker.enable = false;
+        games.enable = false;
+      };
+
+      hardware.bolt.enable = false;
     };
   };
 }
