36c7e87774dc8b1d8e855194751526f5e0538e13
cofob <cofob@riseup.net>
2022-08-09 22:06:54 +0700

    Rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,7 @@ let
   users = [ cofob ];
 
   bear = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICxTTHyhhZTc6pw+p55FfRZeuUqvHLy4RGGcZD1m9Nbf";
-  fly = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ1bStwQLap8M/Fx6YYqJ3er9ym08bv1bavoFuy/mBfN";
+  fly = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBxmrSdAJJIUTSBqDqI1nb7qM1bAFKjWLH6HL7gDiXvJ";
   systems = [ bear fly ];
 in
 {
