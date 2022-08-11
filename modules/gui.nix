9fcb5fcffa66de882d69913b1f78b1af49eb6feb
cofob <cofob@riseup.net>
2022-08-11 15:44:24 +0700

    Remove gnome

--- modules/gui.nix
+++ modules/gui.nix
@@ -2,31 +2,6 @@
 
 {
   config = {
-    environment.systemPackages = with pkgs.unstable.gnomeExtensions; [
-      yakuake
-      gsconnect
-    ];
-
-    environment.gnome.excludePackages = with pkgs.unstable.gnome; [
-      yelp
-      gedit
-      cheese
-      epiphany
-      gnome-logs
-      gnome-maps
-      gnome-music
-      simple-scan
-      gnome-clocks
-      gnome-weather
-      gnome-calendar
-      gnome-contacts
-      gnome-calculator
-      gnome-characters
-      gnome-font-viewer
-      pkgs.gnome-photos
-      pkgs.gnome-connections
-    ];
-
     nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
       "nvidia-x11"
       "nvidia-settings"
@@ -40,12 +15,6 @@
         xkbOptions = "grp:caps_toggle";
         videoDrivers = [ "nvidia" ];
         layout = "us,ru";
-      };
-
-      gnome = {
-        tracker-miners.enable = false;
-        tracker.enable = false;
-        games.enable = false;
       };
 
       hardware.bolt.enable = false;
