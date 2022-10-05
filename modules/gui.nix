11933c5fb4c89b04a7a57790f3ed427ec13a68ef
Egor Ternovoy <cofob@riseup.net>
2022-10-05 17:50:51 +0700

    Update gui

--- modules/gui.nix
+++ modules/gui.nix
@@ -1,44 +1,42 @@
 { config, pkgs, lib, ... }:
 
 {
-  config = {
-    services = {
-      xserver = {
-        enable = true;
-        desktopManager.gnome.enable = true;
-        displayManager.gdm.enable = true;
-        xkbOptions = "grp:caps_toggle";
-        layout = "us,ru";
-      };
-
-      hardware.bolt.enable = false;
-
-      flatpak.enable = true;
-
-      pipewire.enable = true;
-    };
-
-    hardware.opengl.enable = true;
-
-    fonts.fonts = with pkgs; [
-      fira-code-symbols
-      noto-fonts-emoji
-      noto-fonts-cjk
-      liberation_ttf
-      proggyfonts
-      noto-fonts
-      fira-code
-      dina-font
-    ];
-    fonts.fontconfig.enable = true;
-
-    hardware.pulseaudio = {
+  services = {
+    xserver = {
       enable = true;
-      package = pkgs.pulseaudioFull;
+      desktopManager.gnome.enable = true;
+      displayManager.gdm.enable = true;
+      xkbOptions = "grp:caps_toggle";
+      layout = "us,ru";
     };
-    sound.enable = true;
-    nixpkgs.config.pulseaudio = true;
 
-    programs.steam.enable = true;
+    hardware.bolt.enable = false;
+
+    flatpak.enable = true;
+
+    pipewire.enable = true;
   };
+
+  hardware.opengl.enable = true;
+
+  fonts.fonts = with pkgs; [
+    fira-code-symbols
+    noto-fonts-emoji
+    noto-fonts-cjk
+    liberation_ttf
+    proggyfonts
+    noto-fonts
+    fira-code
+    dina-font
+  ];
+  fonts.fontconfig.enable = true;
+
+  hardware.pulseaudio = {
+    enable = true;
+    package = pkgs.pulseaudioFull;
+  };
+  sound.enable = true;
+  nixpkgs.config.pulseaudio = true;
+
+  services.mullvad-vpn.enable = true;
 }
