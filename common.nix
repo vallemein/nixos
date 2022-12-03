eab1fc08a13e8ddcccb3be43805183a0f39d9c3a
vallemein <119686115+vallemein@users.noreply.github.com>
2022-12-03 15:19:47 +0500

    Update common.nix

--- common.nix
+++ common.nix
@@ -8,7 +8,7 @@
 
   services.earlyoom.enable = true;
 
-  age.secrets.remote-builder.file = ./secrets/credentials/remote-builder.age;
+ # age.secrets.remote-builder.file = ./secrets/credentials/remote-builder.age;
 
   nix = {
     settings = {
@@ -33,32 +33,18 @@
       dates = [ "weekly" ];
     };
 
-    buildMachines = [
-      {
-        sshKey = config.age.secrets.remote-builder.path;
-        system = "x86_64-linux";
-        sshUser = "builder";
-        hostName = "10.3.7.30";
-        maxJobs = 3;
-        supportedFeatures = [
-          "big-parallel"
-        ];
-      }
-    ];
-
     distributedBuilds = false;
   };
 
-  programs.ssh.knownHosts = {
-    "10.3.7.30".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKoFVvggf2o3DQsvdAKrfbGMVnly6AmzW/Sebt+1fUW";
-  };
+  #programs.ssh.knownHosts = {
+   # "10.3.7.30".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKoFVvggf2o3DQsvdAKrfbGMVnly6AmzW/Sebt+1fUW";
+  #};
 
   system = {
     stateVersion = "22.11";
     autoUpgrade = {
       enable = true;
       allowReboot = false;
-      flake = "github:cofob/nixos";
       dates = "4:45";
     };
   };
@@ -76,49 +62,26 @@
     };
   };
 
-  environment.systemPackages = with pkgs; let
-    upgrade-system = pkgs.writeScriptBin "upgrade-system" ''
-      sudo rm -rf /root/.cache && sudo nixos-rebuild switch --flake "github:cofob/nixos.git?ref=master"
-    '';
-  in
-  [
+  environment.systemPackages = with pkgs; [
     upgrade-system
     git
     vim
     htop
     ncdu
     tmux
-    terminus_font
-    pastebinit
     wget
-    ffsend
     unzip
+    atool
     # C/CPP/RUST
-    cmake
     clang
-    ninja
     gcc
     pkg-config
-    mold
-    # Python
-    python3
-    python3Packages.jedi-language-server
-    python3Packages.flake8
-    python3Packages.isort
-    python3Packages.autopep8
-    mypy
-    poetry
-    black
-    # Android
-    android-tools
-    ungoogled-chromium
   ];
 
   environment.variables = {
     CMAKE_CXX_COMPILER = "gcc";
-    EDITOR = "code";
-    VISUAL = "code";
-    CHROME_EXECUTABLE = "chromium";
+    EDITOR = "vim";
+    VISUAL = "vim";
   };
 
   environment.shellAliases = {
@@ -129,7 +92,6 @@
     find_port = "sudo netstat -tulnp | grep";
     find_proc = "sudo ps -aux | grep";
     shell = "nix-shell --run 'zsh' -p";
-    please = "sudo";
     brightness = "xrandr --output eDP-1 --brightness";
   };
 
@@ -146,24 +108,19 @@
     XDG_CONFIG_HOME = "$HOME/.config";
   };
 
-  age.secrets.password-cofob.file = ./secrets/passwords/cofob.age;
   users = {
     mutableUsers = false;
     defaultUserShell = pkgs.zsh;
-    users.cofob = {
-      description = "Egor Ternovoy";
+    users.vad = {
+      description = "vad";
       isNormalUser = true;
       extraGroups = [ "wheel" ];
       uid = 1000;
-      passwordFile = config.age.secrets.password-cofob.path;
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETMEzhigdZelWae3V4tQ7/LXsub39SRG2X+jPMeoHMx"
-      ];
     };
     users.root.initialPassword = "root";
   };
 
-  time.timeZone = "Asia/Krasnoyarsk";
+  time.timeZone = "Asia/Yekaterinburg";
 
-  networking.domain = "cofob.ru";
+  networking.domain = "";
 }
