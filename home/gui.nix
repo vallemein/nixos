77272539fc2c7e3a5279a6f4d67f0f17aa204081
vallemein <119686115+vallemein@users.noreply.github.com>
2022-12-03 14:39:48 +0500

    Update gui.nix

--- home/gui.nix
+++ home/gui.nix
@@ -3,39 +3,19 @@
 {
   imports = [ ./apps.nix ./dev.nix ];
 
-  programs.kitty = {
-    enable = true;
-    font = {
-      name = "DejaVu Sans";
-      size = 10;
-    };
-    settings = {
-      background = "#191919";
-    };
-  };
-
   home.packages = with pkgs; [
     noto-fonts-emoji
     liberation_ttf
     dejavu_fonts
     freefont_ttf
-    meslo-lgs-nf
     gyre-fonts
     unifont
-    discord
-    custom.deta
-    bottles
-    obsidian
-    obs-studio
-    termusic
+    cmus
     mpv
-    krita
     # Screenshots
     maim
     xclip
     xdotool
-    konsole
-    kdenlive
     (pkgs.writeScriptBin "br" "xrandr --output eDP-1 --brightness $1")
   ];
 }
